e8ae1390627e342a1e9740f19e629ecdc062f5fe
kumargit <kumar.c.swain@gmail.com>
2018-02-11 10:38:32 +0530

    template_design_pattern

---
+++ template/designpattern/Game.java
@@ -0,0 +1,30 @@
+package com.template.designpattern;
+
+public abstract class Game {
+	
+	abstract void initialize();
+
+	abstract void startPlay();
+
+	abstract void endPlay();
+
+	// template method
+	public final void play() {
+
+		// initialize the game
+		initialize();
+
+		// start game
+		startPlay();
+
+		// end game
+		endPlay();
+	}
+}
+
+/*In Template pattern, an abstract class exposes defined way(s)/template(s) to execute its methods. Its subclasses can override the method implementation as per need but the invocation is to be in the same way as defined by an abstract class. This pattern comes under behavior pattern category.
+
+Implementation
+We are going to create a Game abstract class defining operations with a template method set to be final so that it cannot be overridden. Cricket and Football are concrete classes that extend Game and override its methods.
+
+TemplatePatternDemo, our demo class, will use Game to demonstrate use of template pattern.*/
